78307e3cd1e70293910d69f0432670c743e7f561
pauline12ish34 <paulineishimwe12@gmail.com>
2024-10-05 11:12:08 +0000

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script that distributes an archive to web servers
 """
 
-from fabric.api import put, run, env
-from os.path import exists
+import os
+from fabric.api import env, run , put
 env.hosts = ['54.235.26.56', '3.92.23.222']
 
-
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+    Distributes an archive to web servers
+    returns: True if all operations have been done correctly, 
+    otherwise returns False.
+    """
+    if not os.path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        file_name = archive_path.split('/')[-1]
+        file_no_extension = file_name.split('.')[0]
+        put(archive_path, f'/tmp/{file_name}')
+        run(f'mkdir -p /data/web_static/releases/{file_no_extension}/')
+        run(f"tar -xzf /tmp/{file_name} -C /data/web_static/releases/{file_no_extension}/")
+        run(f'rm /tmp/{file_name}')
+        run(f'mv /data/web_static/releases/{file_no_extension}/web_static/* /data/web_static/releases/{file_no_extension}/')
+        run(f'rm -rf /data/web_static/releases/{file_no_extension}/web_static')
+        run(f'rm -rf /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{file_no_extension}/ /data/web_static/current')
         return True
     except:
         return False
